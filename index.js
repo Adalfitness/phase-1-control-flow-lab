7f8984b27752611f116eaed4335b23c777fb8460
carlos bernal <bernal199152@yahoo.com>
2024-07-29 16:10:53 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
+if (rideDistance <=400) {
+  return 'This one is on me!'
+}
+else if (rideDistance > 400 && rideDistance <= 2000) {
+  return 'That will be twenty bucks.';
+}
+else if (rideDistance >2500)
+  return 'No can do.';
+ 
+
+else if (rideDistance >2000) 
+  return 'I will gladly take your thirty bucks.';
+
+} 
+
+
+  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.': 'No go.';
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+    return "Thank you so much.";
+  
+    case 'not as generous':
+    return 'Thank you.';
+    
+    default:
+      return 'Bye.';
+  }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
